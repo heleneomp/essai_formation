800898563b252351560cd0c3794d8b8ca608f4b4
helene <helene.pauchet@irap.omp.eu>
2017-03-16 16:28:40 +0100

    correction

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def greetings():
 def repeat(x, callback):
     """Call x times callback"""
     for _ in range(x):
-	callback()
+      callback()
 
 if __name__ == "__main__":
     repeat(3, greetings)
